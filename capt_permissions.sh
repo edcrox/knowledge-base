ac54a1c1e33068dc1027e2054917f00eb4649afb
Edcrox <eddya.0617@gmail.com>
2026-06-28 02:40:40 -0400

    Capturar permisos delicados en el sistema

--- capt_permissions.sh
+++ capt_permissions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #----------------------------------------------------------by Edcrox--------------------------------------------------------------
+
 # Este script busca:
 # - Archivos SUID/4000
 # - Directorios SGID/2000
@@ -22,20 +23,42 @@ grayColour="\\e[0;37m\\033[1m"
 #---------------------------------------------------------------------------------------------------------------------------------
 
 echo -e "\
-  ${yellowColour}Este ${endColour}${redColour}script ${endColour}${yellowColour}busca:${endColour}
+${yellowColour}Este ${endColour}${redColour}script ${endColour}${yellowColour}busca:${endColour}
 
-${greenColour}[+]${endColour} ${grayColour}Archivos${endColour} ${redColour}SUID/4000${endColour}
-${greenColour}[+]${endColour} ${grayColour}Directorios${endColour} ${redColour}SGID/2000${endColour}
-${greenColour}[+]${endColour} ${grayColour}Archivos y Directorios${endColour} ${redColour}rwx-rwx-rwx/777${endColour}
-${greenColour}[+]${endColour} ${grayColour}Archivos y Directorios${endColour} ${redColour}---/---/rwx/007${endColour}
-${greenColour}[+]${endColour} ${grayColour}Capabilites${endColour}\
+${purpleColour}[+]${endColour} ${grayColour}Archivos${endColour} ${redColour}SUID/4000${endColour}
+${purpleColour}[+]${endColour} ${grayColour}Directorios${endColour} ${redColour}SGID/2000${endColour}
+${purpleColour}[+]${endColour} ${grayColour}Archivos y Directorios${endColour} ${redColour}rwx-rwx-rwx/777${endColour}
+${purpleColour}[+]${endColour} ${grayColour}Archivos y Directorios${endColour} ${redColour}---/---/rwx/007${endColour}
+${purpleColour}[+]${endColour} ${grayColour}Capabilites${endColour}\
 "
 
 #----------------------------------------------------Crear directorio principal---------------------------------------------------
 
-rm -rf /logs/Permisos
-mkdir /logs/Permisos
+rm -rf /tmp/Permisos
+mkdir -p /tmp/Permisos
 
 #--------------------------------------------------------Archivos SUID/4000-------------------------------------------------------
 
-find / perm /4000 2>/dev/null > /logs/Permisos/ArSUID
+find / -perm /4000 2>/dev/null > /tmp/Permisos/ArSUID
+
+#------------------------------------------------------Directorios SGID/2000------------------------------------------------------
+
+find / -perm /2000 2>/dev/null > /tmp/Permisos/DirSGID
+
+#---------------------------------------------------Archivos y directorios/777----------------------------------------------------
+
+find / -type f -perm 777 2>/dev/null > /tmp/Permisos/Ar777 
+find / -type d -perm 777 2>/dev/null > /tmp/Permisos/Dir777
+
+#---------------------------------------------------Archivos y directorios/007-----------------------------------------------------
+
+find / -type f -perm 007 2>/dev/null > /tmp/Permisos/Ar007
+find / -type d -perm 007 2>/dev/null > /tmp/Permisos/Dir007
+
+#----------------------------------------------------------Capabilities------------------------------------------------------------
+
+getcap -r / 2>/dev/null > /tmp/Permisos/CAP
+
+#--------------------------------------------------------------END-----------------------------------------------------------------
+echo " "
+echo -e "${yellowColour}Logs guardados en carpeta${endColour} ${redColour}tmp${endColour}"
